eac56e65cb68f660f428298ce2b10ce1230b2bc7
Ndhossain <hhhssnahiid@gmail.com>
2022-11-24 02:31:32 +0600

    Category nav and responsive work done

---
+++ src/Components/Common/Menubar/CategoryMenu.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const CategoryMenu = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: `${process.env.REACT_APP_DEV_SERVER_URL}/categories`,
+        }).then(res => {
+            setCategories(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [])
+    console.log(categories);
+    return (
+        <ul className='flex sm:flex-row flex-col sm:justify-center sm:gap-2 sm:bg-primary sm:text-white text-sm sm:font-bold'>
+            {
+                categories?.map(category => 
+                    <li key={category._id} className='p-2'>
+                        <Link to={`/shop/${category._id}`}>{category.name}</Link>
+                    </li>
+                )
+            }
+        </ul>
+    );
+};
+
+export default CategoryMenu;
--- src/Components/Common/Menubar/Menubar.jsx
+++ src/Components/Common/Menubar/Menubar.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import { FaUserPlus, FaHeart, FaCartPlus } from 'react-icons/fa';
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai';
 import { useState } from 'react';
+import CategoryMenu from './CategoryMenu';
 
 const Menubar = () => {
     const [responsiveNav, setResponsiveNav] = useState(false);
 
     return (
-        <div className='border-b border-primary'>
+        <div>
             <nav className='flex justify-between items-center px-3 py-3 max-w-[1480px] mx-auto'>
                 <h1 className='text-3xl text-primary font-black'>
                     <Link to='/'>
@@ -26,7 +27,7 @@ const Menubar = () => {
                     )}
                 </button>
                 <ul 
-                    className={`flex top-0 sm:flex-row flex-col gap-3 sm:relative fixed sm:translate-x-0 sm:h-min h-screen sm:w-min w-1/2 bg-primary sm:bg-white sm:text-primary text-white duration-300 ${responsiveNav ? 'translate-x-[100%]' : 'translate-x-[200%]'}`}
+                    className={`flex top-0 sm:flex-row flex-col gap-3 sm:relative fixed sm:translate-x-0 sm:h-min h-screen sm:w-min w-1/2 bg-primary sm:bg-white sm:text-primary text-white duration-300 overflow-y-scroll sm:overflow-y-visible ${responsiveNav ? 'translate-x-[100%]' : 'translate-x-[200%]'}`}
                 >
                     <li className='flex items-center mt-14 sm:mt-0'>
                         <Link className='px-2 sm:p-2' to='/'>
@@ -43,7 +44,9 @@ const Menubar = () => {
                         <span className='sm:block hidden'>0</span>
                     </li>
                     <li className='flex items-center relative group'>
-                        <Link className='p-2 hidden sm:block' to='/login'><FaUserPlus size={22} color={responsiveNav ? '#fff' : '#222'} /></Link>
+                        <Link className='p-2 hidden sm:block' to='/login'>
+                            <FaUserPlus size={22} color={responsiveNav ? '#fff' : '#222'} />
+                        </Link>
                         <ul className='sm:p-2 sm:absolute sm:top-9 sm:right-0 sm:w-40 sm:bg-white sm:border sm:border-primary sm:hidden sm:group-hover:block'>
                             <li className='px-2 mb-3 sm:mb-0 sm:p-2 text-sm hover:bg-primary/10'>
                                 <Link className='block' to='/register'>Register</Link>
@@ -53,8 +56,17 @@ const Menubar = () => {
                             </li>
                         </ul>
                     </li>
+                    <li>
+                        <div className='block sm:hidden'>
+                            <h6 className='text-center mb-3 text-sm font-bold'>Categories</h6>
+                            <CategoryMenu />
+                        </div>
+                    </li>
                 </ul>
             </nav>
+            <div className='hidden sm:block'>
+                <CategoryMenu />
+            </div>
         </div>
     );
 };
